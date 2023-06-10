e10c4c417a3b6a84986b75e94962b9a1de4e53f6
ErickMwangi <rickthetri20@gmail.com>
2023-06-10 20:48:18 +0300

    completed assignment

--- lib/models.py
+++ lib/models.py
@@ -1,8 +1,14 @@
 from sqlalchemy import func
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+engine = create_engine('sqlite:///seed_db.db')
+session = sessionmaker(bind=engine)
+session = session()
 
 class Game(Base):
     __tablename__ = 'games'
@@ -14,6 +20,7 @@ class Game(Base):
     price = Column(Integer())
     created_at = Column(DateTime(), server_default=func.now())
     updated_at = Column(DateTime(), onupdate=func.now())
+
 
     def __repr__(self):
         return f'Game(id={self.id}, ' + \
